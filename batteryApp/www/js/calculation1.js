22d92600c45b45f9d4a44619f86547bf5228ef55
SaadSurya <saadsurya@hotmail.com>
2014-02-08 19:46:17 +0500

    Every thing is fine except email and combobox size

--- batteryApp/www/js/calculation1.js
+++ batteryApp/www/js/calculation1.js
@@ -14,63 +14,70 @@ function format(amount)
 $(document).ready(function(){
     
     $('.form-control').keyup(function () {
-        ///////////////////////////////////////////////////////////////////////////////////
-        var total_daily = $('#hoursOfSunPerDay').val() * $('#panel').val();
-        var total_monthly=total_daily * 30;
-		
-        $('#solarDailyWattHours').val(format(total_daily));
-        $('#solarMonthlyWattHours').val(format(total_monthly));
-        if (onedone==1)
-        {
-            var percent = ($('#solarMonthlyWattHours').val().replace(',', '') / $('#loadMonthlyWattHours').val().replace(',', '')) * 100;
-            var info = "";
-            if (percent<100)
-            {
-                info="<p align=center>Your solar power system will power "+format(CurrencyFormatted(percent))+"% of your appliances.</p>";
-            }
-            else if (percent==100)
-            {
-                info="<p align=center>Your solar system will just power your appliances.</p>";
-            }
-            else
-            {
-                info="<p align=center>Congratulations, your solar power system is producing "+format(CurrencyFormatted(percent-100))+"% more than your appliances use.  You can sell the excess energy back to your power supplier.</p>";
-            }
-        }
-        $('#information').html("");
-        $('#information').append(info);
-        twodone = 1;
-        ///////////////////////////////////////////////////////////////////////////////////
-        var watt_hour = eval($('#loadDailyWattHours').val().replace(',','') * 1);
-        var battery_volt = eval($('#battery').val() * 1);
-        var bank_size = (watt_hour / battery_volt) * 1.35;
-        $('#requiredBatterySize').val(format(CurrencyFormatted(bank_size)));
-        ///////////////////////////////////////////////////////////////////////////////////
-        watts = $('#maxExpectedLoad').val();
-        hrs = $('#hours').val();
-        $('#loadDailyWattHours').val(format(watts * hrs));
-        $('#loadMonthlyWattHours').val(format(watts * hrs * 30));
-
-        if (twodone == 1) {
-            var percent = ($('#solarMonthlyWattHours').val().replace(',', '') / $('#loadMonthlyWattHours').val().replace(',', '')) * 100;
-            var info = "";
-            if (percent < 100) {
-                info = "<div class='alert alert-danger'>Your solar power system will power " + format(CurrencyFormatted(percent)) + "% of your appliances.</p>";
-            }
-            else if (percent == 100) {
-                info = "<div class='alert alert-info'>Your solar system will just power your appliances.</div>";
-            }
-            else {
-                info = "<div class='alert alert-success'>Congratulations, your solar power system is producing " + format(CurrencyFormatted(percent - 100)) + "% more than your appliances use.  You can sell the excess energy back to your power supplier.</div>";
-            }
-        }
-        $('#information').html("");
-        $('#information').append(info);
-        onedone = 1;
-        ////////////////////////////////////////////////////////////////////////////////////
+        calculate();
+    });
+    $('.form-control').change(function () {
+        calculate();
     });
 
 });
+
+function calculate() {
+    ///////////////////////////////////////////////////////////////////////////////////
+    var total_daily = $('#hoursOfSunPerDay').val() * $('#panel').val();
+    var total_monthly=total_daily * 30;
+		
+    $('#solarDailyWattHours').val(format(total_daily));
+    $('#solarMonthlyWattHours').val(format(total_monthly));
+    if (onedone==1)
+    {
+        var percent = ($('#solarMonthlyWattHours').val().replace(',', '') / $('#loadMonthlyWattHours').val().replace(',', '')) * 100;
+        var info = "";
+        if (percent<100)
+        {
+            info="<p align=center>Your solar power system will power "+format(CurrencyFormatted(percent))+"% of your appliances.</p>";
+        }
+        else if (percent==100)
+        {
+            info="<p align=center>Your solar system will just power your appliances.</p>";
+        }
+        else
+        {
+            info="<p align=center>Congratulations, your solar power system is producing "+format(CurrencyFormatted(percent-100))+"% more than your appliances use.  You can sell the excess energy back to your power supplier.</p>";
+        }
+    }
+    $('#information').html("");
+    $('#information').append(info);
+    twodone = 1;
+    ///////////////////////////////////////////////////////////////////////////////////
+    var watt_hour = eval($('#loadDailyWattHours').val().replace(',','') * 1);
+    var battery_volt = eval($('#battery').val() * 1);
+    var bank_size = (watt_hour / battery_volt) * 1.35;
+    $('#requiredBatterySize').val(format(CurrencyFormatted(bank_size)));
+    ///////////////////////////////////////////////////////////////////////////////////
+    watts = $('#maxExpectedLoad').val();
+    hrs = $('#hours').val();
+    $('#loadDailyWattHours').val(format(watts * hrs));
+    $('#loadMonthlyWattHours').val(format(watts * hrs * 30));
+
+    if (twodone == 1) {
+        var percent = ($('#solarMonthlyWattHours').val().replace(',', '') / $('#loadMonthlyWattHours').val().replace(',', '')) * 100;
+        var info = "";
+        if (percent < 100) {
+            info = "<div class='alert alert-danger'>Your solar power system will power " + format(CurrencyFormatted(percent)) + "% of your appliances.</p>";
+        }
+        else if (percent == 100) {
+            info = "<div class='alert alert-info'>Your solar system will just power your appliances.</div>";
+        }
+        else {
+            info = "<div class='alert alert-success'>Congratulations, your solar power system is producing " + format(CurrencyFormatted(percent - 100)) + "% more than your appliances use.  You can sell the excess energy back to your power supplier.</div>";
+        }
+    }
+    $('#information').html("");
+    $('#information').append(info);
+    onedone = 1;
+    ////////////////////////////////////////////////////////////////////////////////////
+}
 
 // round down to 2 decimal places function
 function CurrencyFormatted(amount)
--- batteryApp/www/js/calculation2.js
+++ batteryApp/www/js/calculation2.js
@@ -1,33 +1,41 @@
 $(document).ready(function () {
     $('.form-control').keyup(function () {
-        var voc = $('#voc').val();
-        var isc = $('#isc').val();
-        var rating = $('#rating').val();
-        var quantity = $('#quantity').val();
-        var connection = $('#connection').val();
-        var cableLength = $('#cableLength').val();
-        var crossSection = $('#crossSection').val();
-        var cableType = $('#cableType').val();
-        var power = rating * (connection == 1 ? quantity : 1);
-        var maxVoltage = 1.2 * voc * (connection == 1 ? 1 : quantity);
-        var maxCurrent = 1.25 * isc * (connection == 1 ? quantity : 1);
-        var minimumVoltage = maxVoltage + 6;
-        var minimumCurrent = maxCurrent + 2;
-        var pvLosses = 0;
-        var pvPercentLosses = 0;
-        pvLosses = (2 * cableLength * maxCurrent * maxCurrent) / ((cableType==1?56:38) * crossSection);
-        pvPercentLosses = (pvLosses * 100 / power);
-        var fuseCurrent = 1.4 * maxCurrent;
-        $('#power').val(isNaN(power) ? "Rating Required" : power.toFixed(1));
-        $('#maxVoltage').val(maxVoltage.toFixed(1));
-        $('#maxCurrent').val(maxCurrent.toFixed(1));
-        $('#minimumVoltage').val(minimumVoltage.toFixed(1));
-        $('#minimumCurrent').val(minimumCurrent.toFixed(1));
-        $('#pvLosses').val(isNaN(pvLosses) ? "Above inputs are required" : pvLosses.toFixed(1));
-        (pvLosses < 22.6) ? $('#pvLosses').css('background-color', '#f2dede') : $('#pvLosses').css('background-color', '#eee');
-        $('#pvPercentLosses').val(isNaN(pvPercentLosses) ? "Above inputs are required" : pvPercentLosses.toFixed(1));
-        (pvPercentLosses < 1) ? $('#pvPercentLosses').css('background-color', '#f2dede') : $('#pvPercentLosses').css('background-color', '#eee');
-        $('#voltageRating').val(maxVoltage.toFixed(1))
-        $('#fuseCurrent').val(fuseCurrent.toFixed(1))
+        calculate();
+    });
+    $('.form-control').change(function () {
+        calculate();
     });
 });
+function calculate() {
+    var voc = $('#voc').val().replace(',', '');
+    var isc = $('#isc').val().replace(',', '');
+    var rating = $('#rating').val().replace(',', '');
+    var quantity = $('#quantity').val().replace(',', '');
+    var connection = $('#connection').val().replace(',', '');
+    var cableLength = $('#cableLength').val().replace(',', '');
+    var crossSection = $('#crossSection').val().replace(',', '');
+    var cableType = $('#cableType').val().replace(',', '');
+    var power = rating * (connection == 1 ? quantity : 1);
+    var maxVoltage = 1.2 * voc * (connection == 1 ? 1 : quantity);
+    var maxCurrent = 1.25 * isc * (connection == 1 ? quantity : 1);
+    var minimumVoltage = maxVoltage + 6;
+    var minimumCurrent = maxCurrent + 2;
+    var pvLosses = 0;
+    var pvPercentLosses = 0;
+    pvLosses = (2 * cableLength * maxCurrent * maxCurrent) / ((cableType==1?56:38) * crossSection);
+    pvPercentLosses = (pvLosses * 100 / power);
+    var fuseCurrent = 1.4 * maxCurrent;
+    $('#power').val(isNaN(power) ? "Rating Required" : format(power.toFixed(1)));
+    $('#maxVoltage').val(format(maxVoltage.toFixed(1)));
+    $('#maxCurrent').val(format(maxCurrent.toFixed(1)));
+    $('#minimumVoltage').val(format(minimumVoltage.toFixed(1)));
+    $('#minimumCurrent').val(format(minimumCurrent.toFixed(1)));
+    $('#pvLosses').val(isNaN(pvLosses) ? "Above inputs are required" : format(pvLosses.toFixed(1)));
+    (pvLosses > 22.6) ? $('#pvLosses').css('background-color', '#f2dede') : $('#pvLosses').css('background-color', '#eee');
+    $('#pvPercentLosses').val(isNaN(pvPercentLosses) ? "Above inputs are required" : format(pvPercentLosses.toFixed(1)));
+    (pvPercentLosses > 1) ? $('#pvPercentLosses').css('background-color', '#f2dede') : $('#pvPercentLosses').css('background-color', '#eee');
+    $('#voltageRating').val(format(maxVoltage.toFixed(1)));
+    $('#fuseCurrent').val(format(fuseCurrent.toFixed(1)));
+}
+function format(amount)
+{ return String(amount).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"); }
